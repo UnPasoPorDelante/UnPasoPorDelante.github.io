57ca8f6cb3df6e248eaabd084e4ee1020084be2b
UnPasoPorDelante <kistokisto@gmail.com>
2023-08-22 10:20:30 +0200

    Google verification

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ export default class Body extends React.Component {
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
+                    <meta name="google-site-verification" content="Y-c10L5_Cp-Lv4RbSBCyPIUQMbAvR7a426h2QVA5mPY" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null)}/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
